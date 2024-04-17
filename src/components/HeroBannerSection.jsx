8dbfa2845b8414e0b1d7891356a0d9593b697b65
KunalKing <kunalprasad957@gmail.com>
2024-04-18 03:16:21 +0530

    New add Images

--- src/components/HeroBannerSection.jsx
+++ src/components/HeroBannerSection.jsx
@@ -1,6 +1,8 @@
 import BannerSyle  from './HeroBannerSection.module.css';
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react"
+import ExplorebuttonImage from '../assets/Explore_button.svg';
+import newEarth from '../assets/NewEarth.svg';
 
 const HeroBannerSection = () => {
 
@@ -14,15 +16,20 @@ const HeroBannerSection = () => {
     return(
         <div className={BannerSyle.wrappetext}>
             <div className={`openanimationUpwards ${BannerSyle.animationIntro} ${BannerSyle.Introtext}`}>
-            <button>Explore the world</button>
+            <button>Explore the world    
+             <img src={ExplorebuttonImage} alt="Logo" />
+             </button>
 
-            <h1>Travel top destination of the world</h1>
+            <h1>Travel <span className={BannerSyle.VibrantText}>top destination</span> of the world</h1>
 
-            <p>we always make our customer happy by providing as many choices as possible.</p>
-            <div>
+            <p className={BannerSyle.paragraphText}>we always make our customer happy by providing as many choices as possible.</p>
+            <div className={BannerSyle.wrapperButtonOpreations}>
                 <button>Get Started</button>
                 <button>Watch Demo</button>
             </div>
+            </div>
+            <div className={BannerSyle.ImagesAnimation}>
+            <img className={BannerSyle.FastTrack} src={newEarth} alt="Logo" />
             </div>
         </div>
     );
