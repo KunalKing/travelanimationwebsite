3c93c4619812a44b83f1e0869b053b0db87a9214
KunalKing <kunalprasad957@gmail.com>
2024-04-21 14:07:07 +0530

    Working Plane animation

--- src/components/HeroBannerSection.jsx
+++ src/components/HeroBannerSection.jsx
@@ -11,23 +11,31 @@ const HeroBannerSection = () => {
 
     gsap.registerPlugin(useGSAP);
     gsap.registerPlugin(MotionPathPlugin);
-
      const runAnimation = () => {
-      gsap.to(".ImageEarth", {opacity:1 ,rotation: 24, duration: 50,
+      gsap.to(".ImageEarth", {opacity:1 ,rotation: 17, duration: 20,
         onComplete: () => {
             gsap.to(".ImageEarth", {
-               opacity: 0,  duration: 5,  rotation: 0,
+               opacity: 1,  duration: 1,  rotation: 0,
                onComplete: runAnimation, });
         }
     });
-    gsap.to(".PathUSA", {position:absolute,opacity:1 ,rotation: 24, duration: 50,
+    gsap.to(".pathUSAanimated", {
+      opacity: 1,
+      rotation: "+=17", // Rotate anticlockwise by 20 degrees
+      duration: 20,
       onComplete: () => {
-          gsap.to(".PathUSA", {
-             opacity: 0,  duration: 5,  rotation: 0,
-             onComplete: runAnimation, });
+        gsap.to(".pathUSAanimated", {
+          opacity: 1,
+          duration: 1,
+          rotation: 0, // Rotate back to initial value
+          onComplete: runAnimation
+        });
       }
-  });
-     }
+    });
+    
+    
+    
+  }
   
     useGSAP(() => {
 
@@ -57,31 +65,53 @@ const HeroBannerSection = () => {
         //     }
         //   });
 
-        // gsap.to("#rect", {
-        //     duration: 5, 
-        //     repeat: 12,
-        //     repeatDelay: 1,
-        //     yoyo: true,
-        //     ease: "power1.inOut",
-        //     motionPath:{
-        //       path: "#path",
-        //       align: "#path",
-        //       autoRotate: true,
-        //       alignOrigin: [0.5, 0.5]
-        //     }
-        //   });
+
+// Working Plane Animation Code
+        function test() {
+          animateSVG(); 
+          gsap.to(".hello", {
+            rotation: 33,
+            duration: 1,  
+          });
+        }
+
+        function animateSVG() {
+          gsap.to("#rect", {
+            duration: 15,
+            //repeat: -1, // Set repeat to -1 for infinite looping
+            repeatDelay: 1,
+            yoyo: true,
+            ease: "power1.inOut",
+            motionPath: {
+              path: "#path",
+              align: "#path",
+              autoRotate: true,
+              alignOrigin: [0.5, 0.5],
+            },
+            onComplete: function() {
+              test();
+            }
+          });
+        }
+        
+        animateSVG(); 
+
+        
+       
       
-     
+        
+        
     });
 
     useGSAP(runAnimation);
 
+   
+
+
     return(
         <div className={BannerSyle.wrappetext}>
-            <div className={`openanimationUpwards ${BannerSyle.animationIntro} ${BannerSyle.Introtext}`}>
-            <button>Explore the world    
-             <img src={ExplorebuttonImage} alt="Logo" />
-             </button>
+            <div className={`openanimationUpwards ${BannerSyle.animationIntro} ${BannerSyle.Introtext}`} >
+            <button>Explore the world<img src={ExplorebuttonImage} alt="Logo" /></button>
 
             <h1>Travel <span className={BannerSyle.VibrantText}>top destination</span> of the world</h1>
 
@@ -94,22 +124,19 @@ const HeroBannerSection = () => {
             <div className={BannerSyle.ImagesAnimation}>
             <img className={`Plane ${BannerSyle.planeImage}`} src={Plane} alt="plane" />
             <img className={`ImageEarth ${BannerSyle.FastTrack}`} src={newEarth} alt="Logo" />
-            {/* <img className={`PathOne ${BannerSyle.PathUSA}`} src={pathOne} alt="pathUSA" /> */}
+           
 
-<svg className={BannerSyle.PathUSA} width="50%" height="auto" viewBox="-20 0 557 190" id="svg" fill="none" stroke="black" strokeWidth="2">
+            <svg className={`pathUSAanimated ${BannerSyle.PathUSA}`} width="50%" height="auto" viewBox="-20 0 557 190" id="svg" fill="none" stroke="black" strokeWidth="2">
   <rect stroke="none" fill="none"/>
-  <path  id="path" d="m9,100 C 370,180 470,180 610,100"/>
+  <path id="path" d="m9,100 C 370,180 470,180 610,100"/>
   <g id="rect">
-    <rect width="85" height="30" fill="dodgerblue" />
-    <text x="10" y="19" font-size="14">SVG &lt;rect&gt;</text>
+    <image className={`rect hello ${BannerSyle.Planepathabove}`} href={Plane} alt="plane" />
   </g>
 </svg>
 
-
-
-
             </div>
         </div>
+        
     );
 
 }
